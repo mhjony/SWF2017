30017ba8cdec0c9581e91b6b0709dc7dbe3f7a83
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2017-03-09 22:21:20 +0200

    path direction and login system has been added

---
+++ LoginSystem/Assets/LoginScript/Login.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public Texture2D backgroundImg;
+public GUISkin skin;
+
+public class Login : MonoBehaviour {
+#region variables
+	//Static Vatiable
+	public static string Email = "";
+	public static string Passwordp = "";
+	//public variable
+	public string CurrentMenu = "Login";
+
+
+	//Private Variable
+	private string CreateAcountUrl = "";
+	private string LoginUrl = "";
+
+	//GuI Test section
+	public float X;
+	public float Y;
+	public float Width;
+	public float Height;
+
+
+#endregion 
+
+	// Use this for initialization
+	void Start () {
+		
+	} // End start method
+
+	//Main GUI Function
+	void onGUI(){
+		if (CurrentMenu == "CreateAccount") {
+			LoginGUI ();
+		} else if (CurrentMenu == "CreateAccount") {
+			CreateAccountGUI ();
+		}
+
+	} //End onGUI
+
+#region custon methods
+	//This method will login the accounts
+	void LoginGUI(){
+		GUI.Box(new Rect(320, 120, (Screen.width/4)+400, (Screen.height/4)+350), "");
+		
+	}//End Login GUI
+
+
+	// This method will be the GUI for creating the account
+	void CreateAccountGUI(){
+	
+	}//End create account GUI
+#endregion
+	
+
+}//End class
