130615a73653c9d11080187facb78b0bf1b342c4
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2017-03-22 04:19:05 +0200

    Log in and account creation works fine

--- LoginSystem/Assets/LoginAccountT.php
+++ LoginSystem/Assets/LoginAccountT.php
@@ -10,45 +10,19 @@ mysql_select_db($DBName) or die("Can't connect into database");
 $Email = $_REQUEST["Email"];
 $Password = $_REQUEST["Password"];
 
-if (!$Email || !$Password) 
-	{
-	# code...
-	echo "Login or user can't be empty";
-	}
-	else
-	{
-	$SQL = "SELECT *FROM accounts WHERE Email = '" . $Email . "'";
-	$result_id = @mysql_query($SQL) or die("Database Error");
-	$total = mysql_num_rows($result_id);
-	if ($total) 
-	{
-		# code...
-		$datas = @mysql_fetch_array($result_id);
-		if (strcmp($Password, $datas["Password"])) 
-		{
-			# code...
-			$sql2 = "SELECT Characters FROM accounts WHERE Email = '" . $Email . "'";
-			$result_id2 = @mysql_query($sql2) or die("Database error!");
-			while ($row = mysql_fetch_array($result_id2)) 
-			{
-				# code...
-				echo $row['Characters'];
-				echo ":";
-				echo "Sucess";
-			}
+$result = mysql_query("select * from accounts where Email = '$Email' and Password = '$Password'")
+			or die("Failed to query database".mysql_error());
 
-		}
-		else
-		{
-			echo "Wrong Password";
-		}
-	}
-	
-	else
-	{
-		echo "Name Does not exist";
-	}
+$row = mysql_fetch_array($result);
+if ($row['Email'] == $Email && $row['Password'] == $Password) {
+	echo "Login success!! Congratulations";
 }
+
+else{
+	echo "Login Fail";
+}
+
+
 
 
 mysql_close();
